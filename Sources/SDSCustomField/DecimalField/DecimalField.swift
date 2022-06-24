92733fec272e107aeb31cd431c83f47d880756fb
Tomoaki Yagishita <tyagishi@gmail.com>
2022-06-24 21:15:03 +0900

    onEditingChanged is same with using onChange(focus)

--- Sources/SDSCustomField/DecimalField/DecimalField.swift
+++ Sources/SDSCustomField/DecimalField/DecimalField.swift
@@ -29,9 +29,7 @@ public struct DecimalField<T: StringProtocol>: View {
                 viewModel.fieldString
             }, set: { newValue in
                 viewModel.updateFieldString(newValue)
-            }), onEditingChanged: { focus in
-                if !focus { apply() }
-            })
+            }))
             .multilineTextAlignment(.trailing)
             .focused($fieldFocus)
             .onSubmit{ apply() }
